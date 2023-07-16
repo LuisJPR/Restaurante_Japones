494e31ee806c107983adab58c063f0b51b5cefc6
JStk-Dev <danielzegarra12310@gmail.com>
2023-07-16 18:30:52 -0500

    add error 404 page and update file PHP

--- PHP/update_data.php
+++ PHP/update_data.php
@@ -7,7 +7,7 @@
     $clave = $_POST["clave"];
     $direccion = $_POST["direccion"];
 
-    // Obtener los datos de sesion activos
+    // Obtener los datos de sesión activos
     $session_nomb = $_SESSION["nombre"];
     $session_email = $_SESSION["correo"];
 
@@ -19,22 +19,67 @@
     // Realizar las validaciones necesarias
     include("util.php");
     if (validarRegistroForm($nombre, $correo)) {
-        
-        // Actualiza la informacion en la base de datos
-        $sql = "UPDATE usuarios SET nombre = '$nombre', correo = '$correo', clave = '$clave', direccion = '$direccion'
-        WHERE nombre = '$nomb' AND correo ='$email'";
+
+        // Verificar si existen coincidencias en la base de datos
+        $coincidenciaNombre = false;
+        $coincidenciaCorreo = false;
+        findMatches($con, $nombre, $correo);
+
+        // Si no hay coincidencias, actualizar la información en la base de datos
+        if (!$coincidenciaNombre && !$coincidenciaCorreo) {
+            $sql = "UPDATE usuarios SET nombre = '$nombre', correo = '$correo', clave = '$clave', direccion = '$direccion'
+            WHERE nombre = '$session_nomb' AND correo = '$session_email'";
+
+            $result = mysqli_query($con, $sql);
+            if (!$result) {
+                $response = [
+                    "message" => "Error al actualizar los datos en el servidor."
+                ];
+            } else {
+                // Actualizar los datos de sesión con los nuevos valores
+                $_SESSION["nombre"] = $nombre;
+                $_SESSION["correo"] = $correo;
+
+                $response = [
+                    "message" => "Los datos se actualizaron correctamente en el servidor."
+                ];
+            }
+        } else {
+            $response = [
+                "message" => "Ya existe una cuenta registrada en el sistema con los datos ingresados. Modifique su información e intente de nuevo."
+            ];
+        }
+    } else {
+        $response = [
+            "message" => "Error en los datos enviados desde el cliente."
+        ];
     }
 
+    // Función para buscar coincidencias en la base de datos
+    function findMatches($con, $session_nomb, $session_email) {
+        $query = "SELECT correo, nombre FROM usuarios";
+        $result = mysqli_query($con, $query);
+        if (!$result) {
+            die("Error al obtener los registros: " . mysqli_error($con));
+        }
 
-    function findMatches($session_nomb, $session_email) {
+        // Verificar coincidencias de nombre y correo
+        global $coincidenciaNombre, $coincidenciaCorreo;
+        $coincidenciaNombre = false;
+        $coincidenciaCorreo = false;
 
+        while ($row = mysqli_fetch_assoc($result)) {
+            if ($row['nombre'] == $session_nomb) {
+                $coincidenciaNombre = true;
+            }
+
+            if ($row['correo'] == $session_email) {
+                $coincidenciaCorreo = true;
+            }
+        }
     }
 
     // Devolver una respuesta al cliente
-    $response = [
-        "message" => "--- Los datos se guardarobn correctamente en el servidor ---"
-    ];
-
     header("Content-Type: application/json");
     echo json_encode($response);
 ?>
